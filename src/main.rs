0723e3dfecaeddd27edc5bc6bcd204f937dafde6
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-07-02 18:40:05 -0400

    :sparkles: feat(apply button): adjust sizing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod utils;
 mod widgets;
 mod window;
-use gtk::Application;
 use gtk::prelude::*;
+use gtk::{Application, Box};
 use widgets::apply_button::ApplyButton;
 use widgets::header::Header;
 use window::MainWindow;
@@ -16,8 +16,12 @@ fn main() {
         let win = MainWindow::new(app);
         let header = Header::new();
         let apply_button = ApplyButton::new();
+        let main_box = Box::new(gtk::Orientation::Vertical, 50);
+        main_box.append(apply_button.as_ref());
+
+        apply_button.set_halign(gtk::Align::End); // Or Start/End based on your needs
         win.set_titlebar(Some(header.as_ref()));
-        win.set_child(Some(apply_button.as_ref()));
+        win.set_child(Some(&main_box));
         win.show();
     });
 
--- src/widgets/apply_button.rs
+++ src/widgets/apply_button.rs
@@ -1,11 +1,13 @@
 use crate::utils::widget_wrapper::WidgetWrapper;
-use gtk::Button;
+use gtk::{Button, prelude::WidgetExt};
 
 pub type ApplyButton = WidgetWrapper<gtk::Button>;
 
 impl ApplyButton {
     pub fn new() -> Self {
         let button = Button::with_label("Apply");
+        button.set_size_request(100, 40);
+        button.add_css_class("suggested-action");
         Self { inner: button }
     }
 }

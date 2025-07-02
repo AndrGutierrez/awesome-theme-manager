fb481dc2370550317591cfe517a99f32c9666610
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-07-02 18:40:05 -0400

    :sparkles: feat(apply button): adjust sizing

--- src/widgets/apply_button.rs
+++ src/widgets/apply_button.rs
@@ -1,11 +1,12 @@
 use crate::utils::widget_wrapper::WidgetWrapper;
-use gtk::Button;
+use gtk::{Button, prelude::WidgetExt};
 
 pub type ApplyButton = WidgetWrapper<gtk::Button>;
 
 impl ApplyButton {
     pub fn new() -> Self {
         let button = Button::with_label("Apply");
+        button.set_size_request(1, 50);
         Self { inner: button }
     }
 }

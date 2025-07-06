bef670f55acb77dc42f95f07e9f3e60238f71340
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-07-06 18:34:01 -0400

    :sparkles: feat(theme_selection): working

--- src/widgets/apply_button.rs
+++ src/widgets/apply_button.rs
@@ -3,56 +3,55 @@ use gtk::{
     Button,
     prelude::{ButtonExt, WidgetExt},
 };
-use std::process::Command;
 use std::thread;
 
+use std::process::Command;
 pub type ApplyButton = WidgetWrapper<gtk::Button>;
 
 use fs_extra::dir::{CopyOptions, copy};
+use std::env;
 use std::fs;
 use std::path::Path;
 
 fn sync_theme(source: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Construct the full source path: ".awesome-themes/{source}"
-    let src_path = Path::new(".awesome-themes").join(source);
-    println!("{:?}", src_path.to_str());
+    let home = env::home_dir().expect("Could not find home directory");
+    let mut src_path = Path::new(".awesome-themes").join(source);
+    src_path = home.join(src_path);
 
-    // Verify source exists and is a directory
     if !src_path.exists() || !src_path.is_dir() {
         //return Err(format!("Theme '{}' not found in .awesome-themes/", source).into());
     }
 
-    let dest_path = Path::new(".config/awesome");
+    let mut dest_path = Path::new(".config/awesome").join("");
+    dest_path = home.join(dest_path);
 
-    // Clear destination directory by removing and recreating it
     if dest_path.exists() {
-        fs::remove_dir_all(dest_path)?;
+        fs::remove_dir_all(&dest_path)?;
     }
-    fs::create_dir_all(dest_path)?;
+    fs::create_dir_all(&dest_path)?;
 
-    // Copy all contents from source to destination
     let mut options = CopyOptions::new();
-    options.content_only = true; // Don't copy the directory itself, just its contents
+    options.content_only = true;
     options.overwrite = true;
 
-    copy(&src_path, dest_path, &options)?;
+    copy(&src_path, &dest_path, &options)?;
 
     Ok(())
 }
 
 fn change_theme(theme_name: &str) {
     sync_theme(theme_name);
-    //Command::new("awesome-client")
-    //    .arg("awesome.restart()")
-    //    .status()
-    //    .expect("Failed to restart Awesome WM");
+    Command::new("awesome-client")
+        .arg("awesome.restart()")
+        .status()
+        .expect("Failed to restart Awesome WM");
 }
 impl ApplyButton {
     pub fn new() -> Self {
         let button = Button::builder().label("Apply").build();
         button.connect_clicked(move |button| {
             thread::spawn(move || {
-                change_theme("multicolor");
+                change_theme("void-heart");
             })
             .join()
             .unwrap();

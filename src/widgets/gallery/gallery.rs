e95b54096d4d86a606c71652ce15e05c98fa1294
Andres Gutierrrez <andresgutierrezdev@gmail.com>
2025-07-31 22:08:47 -0400

    :sparkles: feat(gallery): automatic column arrangement

--- src/widgets/gallery/gallery.rs
+++ src/widgets/gallery/gallery.rs
@@ -11,15 +11,24 @@ impl Gallery {
         let itm2 = Item::new().inner;
         let itm3 = Item::new().inner;
         let itm4 = Item::new().inner;
+        let items = [itm1, itm2, itm3, itm4];
         let bx = Grid::builder()
             .margin_start(0)
             .column_spacing(10)
             .row_spacing(10)
             .build();
-        bx.attach(itm2.as_ref(), 1, 0, 1, 1);
-        bx.attach(itm1.as_ref(), 2, 0, 1, 1);
-        bx.attach(itm3.as_ref(), 3, 0, 1, 1);
-        bx.attach(itm4.as_ref(), 1, 2, 1, 1);
+
+        let mut column_count = 0;
+        let mut row_count = 0;
+        let max_columns = 3;
+        for itm in items {
+            if (column_count % max_columns) == 0 {
+                row_count += 1;
+                column_count = 0;
+            }
+            bx.attach(itm.as_ref(), column_count, row_count, 1, 1);
+            column_count += 1;
+        }
         Self {
             inner: WidgetWrapper { inner: bx },
         }
